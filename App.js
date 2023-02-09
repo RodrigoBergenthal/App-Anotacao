b5153c83d7498793129cf098cebeb1c8959acbdf
RodrigoBergenthal <rodrigo_bergenthal@live.com>
2023-02-09 08:47:14 -0300

    finalizando com AsyncStorage

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AntDesign } from '@expo/vector-icons';
+import { AsyncStorage } from 'react-native';
 import { useFonts, Bangers_400Regular } from '@expo-google-fonts/bangers';
 import AppLoading from 'expo-app-loading';
-import { StyleSheet, Text,TextInput, View, ImageBackground, TouchableOpacity, ScrollView, Modal, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, TextInput, View, ImageBackground, TouchableOpacity, ScrollView, Modal, TouchableHighlight } from 'react-native';
 
 
 export default function App() {
@@ -11,26 +12,27 @@ export default function App() {
   const image = require('./resources/bg.jpg');
   console.disableYellowBox = true;
 
-  const [tarefas, setarTarefas] = useState([
-    {
-      id: 1,
-      tarefa: 'Minha primeir Tarefa 1.'
-    },
-    {
-      id: 2,
-      tarefa: 'Minha segunda tarefa...'
-
-    },
-    {
-      id: 3,
-      tarefa: 'Minha outra tarefa...'
-
-    },
-    
-  ]);
+  const [tarefas, setarTarefas] = useState([]);
 
   const [modal, setModal] = useState(false);
 
+  const [tarefaAtual, setTarefaAtual] = useState('');
+
+  useEffect(() => {
+    //alert('app carregando...');
+
+    (async () => {
+      try {
+        let tarefasAtual = await AsyncStorage.getItem('tarefas');
+        if (tarefasAtual == null)
+          setarTarefas([]);
+        else
+          setTarefa(JSON.parse(tarefasAtual));
+      } catch (error) {
+        //error saving data
+      }
+    })();
+  }, [])
 
   let [fontsLoaded] = useFonts({
     Bangers_400Regular,
@@ -39,21 +41,57 @@ export default function App() {
     return <AppLoading />;
   }
 
+
+
   function deletarTarefa(id) {
-    alert('Tarefa com ID '+ id + ' foi deletada com sucesso!')
+    alert('Tarefa com ID ' + id + ' foi deletada com sucesso!')
     //TODO: deletar do array/estado a tarefa com id especificado!
 
-    let newTarefas = tarefas.filter(function(val){
+    let newTarefas = tarefas.filter(function (val) {
       return val.id != id;
     });
 
-      setarTarefas(newTarefas);
+    setarTarefas(newTarefas);
+
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas));
+        console.log('chamando');
+      } catch (error){
+        //Error saving data
+      }
+    })();
   }
+
+  function addTarefa() {
+    setModal(!modal);
+
+    let id = 0;
+    if (tarefas.length > 0) {
+      id = tarefas[tarefas.length - 1].id + 1;
+    }
+
+    let tarefa = { id: id, tarefa: tarefaAtual };
+
+    setarTarefas([...tarefas, tarefa]);
+
+
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas));
+        //console.log('chamando');
+      } catch (error){
+        //Error saving data
+      }
+    })();
+  };
+
+
 
 
   return (
 
-    <ScrollView style={{flex:1}}>
+    <ScrollView style={{ flex: 1 }}>
       <StatusBar hidden />
       <Modal
         animationType="slide"
@@ -65,13 +103,11 @@ export default function App() {
       >
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-          <TextInput autoFocus={true}></TextInput>
+            <TextInput onChangeText={text => setTarefaAtual(text)} autoFocus={true}></TextInput>
 
             <TouchableHighlight
               style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-              onPress={() => {
-                setModal(!modal);
-              }}
+              onPress={() => addTarefa()}
             >
               <Text style={styles.textStyle}>Adicionar Tarefa</Text>
             </TouchableHighlight>
@@ -99,8 +135,8 @@ export default function App() {
           </View>);
         })
       }
-      <TouchableOpacity style={styles.btnAddTarefa} onPress ={() => setModal(true)}><Text
-      style={{textAlign:'center',color:'white'}}>Adicionar Tarefa</Text></TouchableOpacity>
+      <TouchableOpacity style={styles.btnAddTarefa} onPress={() => setModal(true)}><Text
+        style={{ textAlign: 'center', color: 'white' }}>Adicionar Tarefa</Text></TouchableOpacity>
 
     </ScrollView>
   );
@@ -137,7 +173,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:'rgba(0,0,0,0.5)'
+    backgroundColor: 'rgba(0,0,0,0.5)'
   },
   modalView: {
     margin: 20,
@@ -153,7 +189,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    zIndex:5
+    zIndex: 5
   },
   openButton: {
     backgroundColor: "#F194FF",
@@ -170,11 +206,11 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     textAlign: "center"
   },
-  btnAddTarefa:{
-    width:200,
-    padding:8,
-    backgroundColor:'grey',
-    marginTop:20,
+  btnAddTarefa: {
+    width: 200,
+    padding: 8,
+    backgroundColor: 'grey',
+    marginTop: 20,
   }
 
 })
